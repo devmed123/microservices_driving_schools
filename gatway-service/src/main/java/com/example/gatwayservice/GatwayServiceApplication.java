fb889f82861b3765ea57c9e2eca3d5d92c210865
devmed123 <mohammed.bouzit1@usmba.ac.ma>
2022-12-20 23:52:47 +0100

    first commit

---
+++ gatway-service/src/main/java/com/example/gatwayservice/GatwayServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.gatwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayServiceApplication.class, args);
+    }
+  @Bean
+  DiscoveryClientRouteDefinitionLocator dynamicroutes(ReactiveDiscoveryClient rdc , DiscoveryLocatorProperties dlp )
+  {
+      return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+  }
+}
